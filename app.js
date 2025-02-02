6b3e629c6a1314cbde4c975098d3c96917fd7b0d
JoJonior <sampleytext@gmail.com>
2025-02-02 08:37:26 -0300

    adicionado: array array_nomes,  função adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let array_nomes =[];
+
+
+function adicionarAmigo(){
+    let amigoInput = document.getElementById('amigo');
+    
+    if(amigoInput.value!=""){
+        array_nomes.push(amigoInput.value);
+        amigoInput.value='';
+        AtualizarLista();
+        
+        
+    }else{
+        //ERRO
+        alert("Por favor, insira um nome.");
+    }
+    
+    
+
+} 
